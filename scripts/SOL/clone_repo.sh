66395ed223bd61b795e6c107370b484d74a05277
Andy Tsai <andytsai104@gmail.com>
2026-03-31 02:01:01 -0700

    Update clone_repo.sh

--- scripts/SOL/clone_repo.sh
+++ scripts/SOL/clone_repo.sh
@@ -8,4 +8,19 @@
 #SBATCH -o outputs/out/clone_repo_%j.out
 #SBATCH -e outputs/err/clone_repo_%j.err
 
-git clone https://github.com/andytsai104/pedestrian_RL
+set -e
+
+REPO_URL="https://github.com/andytsai104/pedestrian_RL.git"
+REPO_DIR="pedestrian_RL"
+BRANCH="main"
+
+if [ ! -d "$REPO_DIR/.git" ]; then
+    echo "Repo not found. Cloning..."
+    git clone -b "$BRANCH" "$REPO_URL"
+else
+    echo "Repo exists. Force updating..."
+    cd "$REPO_DIR"
+    git fetch origin
+    git reset --hard "origin/$BRANCH"
+    git clean -fd
+fi
