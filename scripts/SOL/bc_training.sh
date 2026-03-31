ff3b55fd31331be95d911b2acf9ff9c67cbb6c8c
Andy Tsai <andytsai104@gmail.com>
2026-03-31 02:10:57 -0700

    Update clone_repo.sh

--- scripts/SOL/bc_training.sh
+++ scripts/SOL/bc_training.sh
@@ -8,8 +8,19 @@
 #SBATCH -o outputs/out/pedestrian_RL/bc_training_%j.out
 #SBATCH -e outputs/err/pedestrian_RL/bc_training_%j.err
 
-# Go to pedestrian_RL project
-cd pedestrian_RL/
+set -e
+
+# Initialize conda
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /scratch/ctsai67/envs/carla_env
+
+# Go to project
+cd ~/pedestrian_RL
+
+# Debug info
+echo "Python: $(which python)"
+python --version
+python -c "import sys; print(sys.executable)"
 
 # Run training
 python -m pedestrian_rl.training.bc_training
