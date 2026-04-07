56840a1b074a56d2b2e2b7edfe07cdd085d4b1f7
Andy Tsai <andytsai104@gmail.com>
2026-04-07 13:19:53 -0700

    Modified bc_training config

--- scripts/SOL/bc_training.sh
+++ scripts/SOL/bc_training.sh
@@ -5,7 +5,7 @@
 #SBATCH -n 1
 #SBATCH -c 8
 #SBATCH -t 0-02:30:00
-#SBATCH --mem=8G
+#SBATCH --mem=20G
 #SBATCH -o outputs/out/pedestrian_RL/bc_training_%j.out
 #SBATCH -e outputs/err/pedestrian_RL/bc_training_%j.err
 #SBATCH --mail-type=ALL
